aeb6d616d87614179bb39cf6b630784ad79d7bfa
Vinh Tieu <tieuvinh32@gmail.com>
2024-01-02 08:48:00 +0700

    Add Register Form

---
+++ src/pages/signUp.jsx
@@ -0,0 +1,57 @@
+import { Button, Input } from "antd";
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <div className="grid h-screen grid-flow-col">
+      <div className="flex flex-col h-full col-auto">
+        <div className="flex-1 text-center w-[25rem] mx-auto flex items-center justify-center flex-col">
+          <span className="block mb-2 text-[32px] leading-10 font-medium">
+            Welcome to monday.com
+          </span>
+          <span className="block mb-12 leading-[30px] font-normal">
+            Get started - it's free. No credit card needed.
+          </span>
+          <div className="px-[.5625rem] space-y-4 w-full">
+            <Button className="flex items-center justify-center w-full h-10 space-x-2">
+              <img
+                src="https://dapulse-res.cloudinary.com/image/upload/remote_logos/995426/google-icon.svg"
+                alt="google icon"
+                className="inline-block w-4 p-0 m-0 text-base aspect-square"
+              />
+              <span className="">Continue with Google</span>
+            </Button>
+            <div className="flex flex-row items-center gap-2">
+              <div className="h-1/2 border border-t-[#d0d4e4] w-full"></div>
+              Or
+              <div className="h-1/2 border border-t-[#d0d4e4] w-full"></div>
+            </div>
+            <Input className="h-10" placeholder="name@company.com"></Input>
+            <Button type="primary" className="w-full bg-[#0073ea] h-10">
+              Continue
+            </Button>
+            <div>
+              <span>By proceeding, you agree to the</span>
+              <div>
+                <a href="#!" className="text-[#5e9cd3] hover:underline">
+                  Terms of Service
+                </a>
+                <span className="px-1">and</span>
+                <a href="#!" className="text-[#5e9cd3] hover:underline">
+                  Privacy Policy
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="inline-block mb-16 text-center">
+          <span className="px-1">Already have an account?</span>
+          <a href="#!" className="text-[#5e9cd3] hover:underline">
+            Log in
+          </a>
+        </div>
+      </div>
+      <div className="col-[40%] bg-red-500"></div>
+    </div>
+  );
+}
